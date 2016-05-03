b31c6dff073318fe46ac09821cde38dee3f718bd
Arghya-019 <arghya0190@gmail.com>
2016-05-03 16:14:33 +0530

    docker container deploy script modified

--- DockerDeploy.bat
+++ DockerDeploy.bat
@@ -1,1 +1,1 @@
-pscp -i cdsimplified.ppk "C:\Program Files\Jenkins\jobs\Validate_Item_gradle\workspace\build\libs\validate_gradle-1.0.war" ubuntu@52.6.70.143:/home/ubuntu
+"C:\Program Files\PSCP\pscp" -i cdsimplified.ppk "C:\Program Files\Jenkins\jobs\Validate_Item_gradle\workspace\build\libs\validate_gradle-1.0.war" ubuntu@52.6.70.143:/home/ubuntu
